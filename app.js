3a28644e8f2df06aa702daab73a0d59edeafd407
leonardoprogramador01 <166336676+leonardoprogramador01@users.noreply.github.com>
2024-04-07 18:13:42 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+let listaNumerosSorteados = [];
+let numeroMaxinoNaLista = 3;
+let numeroSecreto = sortearNumero();
+let tentativas = 1;
+
+
+function textosDoJogo (tag, texto){
+let campos = document.querySelector(tag);
+campos.innerHTML = texto;
+responsiveVoice.speak(texto,'Brazilian Portuguese Female', {rate:1.2});
+}
+
+function mensagemIncial(){
+
+  textosDoJogo('h1', 'Jogo do numero secreto');
+  textosDoJogo('p', 'CHUTE UM NUMERO ENTRE 1 E 10');
+  
+}
+
+mensagemIncial();
+
+function verificarChute(){
+  let chute = document.querySelector('input').value;
+  let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'; 
+
+  if ( chute == numeroSecreto) {
+   textosDoJogo('h1','acertou');
+   textosDoJogo('p', `vc acertou com ${tentativas} ${palavraTentativa}`);
+   document.getElementById('reiniciar').removeAttribute('disabled');
+   limparJogo();
+  }
+
+  else {
+  
+  if (chute<numeroSecreto){
+  textosDoJogo('h1','errou');
+  textosDoJogo('p','numero secreto é maior ');
+  }
+  else{
+  textosDoJogo('h1','errou');
+  textosDoJogo('p','numero secreto é menor');
+  }
+
+  tentativas++;
+  limparJogo();
+}
+
+}
+
+function sortearNumero(){
+  let numeroEscolhido =  parseInt(Math.random() * numeroMaxinoNaLista + 1);
+  let quantidadeDeNumerosNaLista = listaNumerosSorteados.length;
+
+  if (quantidadeDeNumerosNaLista == numeroMaxinoNaLista){
+    listaNumerosSorteados = [];
+
+  }
+
+  if (listaNumerosSorteados.includes(numeroEscolhido)){
+    return sortearNumero();
+  } else {
+    listaNumerosSorteados.push(numeroEscolhido)
+    console.log(listaNumerosSorteados)
+    return numeroEscolhido;
+  }
+ 
+}
+
+
+function limparJogo(){
+
+  chute = document.querySelector('input');
+  chute.value = '';
+
+}
+
+function reiniciarJogo(){
+  numeroSecreto = sortearNumero(); 
+  limparJogo();
+  mensagemIncial();
+  document.getElementById('reiniciar').setAttribute('disabled',true);
+  tentativas = 1;
+ 
+
+}
+
+
+
